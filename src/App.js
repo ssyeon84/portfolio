ad977a5ad2876d7ea103e129349b427c0ce9854e
ssyeon84 <sy.song@nextnow.kr>
2023-04-27 16:05:30 +0900

    부트스트랩 템플릿 디자인 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
-import Layout from './layout/Layout';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Layout></Layout>
+    <div>
+      <Router>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
